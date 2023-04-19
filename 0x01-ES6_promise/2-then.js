26025d07bf1514bc33f8654016689a3da20193d4
bek2518 <bereketb20@hotmail.com>
2023-04-19 15:23:00 +0300

    Typo corrected

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((myresolve, myreject) => {
     if (promise) {
-      myresolve({ status: 200, body: 'Success' });
+      myresolve({ status: 200, body: 'success' });
       console.log('Got a response from the API');
     } else {
       myreject(new Error());
