bcb03fe39e24b5d6b4a55e40fa1be3261edc6585
bek2518 <bereketb20@hotmail.com>
2023-04-19 15:25:10 +0300

    Changed place of log

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,10 +2,9 @@ export default function handleResponseFromAPI(promise) {
   return new Promise((myresolve, myreject) => {
     if (promise) {
       myresolve({ status: 200, body: 'success' });
-      console.log('Got a response from the API');
     } else {
       myreject(new Error());
-      console.log('Got a response from the API');
     }
+    console.log('Got a response from the API');
   });
 }
