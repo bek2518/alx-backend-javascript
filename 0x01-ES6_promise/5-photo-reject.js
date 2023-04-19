7ac6f78be7e3f9e71cd5f5ce5a4b83f809262e3d
bek2518 <bereketb20@hotmail.com>
2023-04-19 16:51:12 +0300

    Changed to reject method

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,3 @@
 export default function uploadPhoto(filename) {
-  return new Promise((myreject) => {
-    myreject(new Error(`${filename} cannot be processed`));
-  });
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
