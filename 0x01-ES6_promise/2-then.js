6f5b72526c71328e28a0bf25d78c6b59d2506cd4
bek2518 <bereketb20@hotmail.com>
2023-04-19 15:13:45 +0300

    Response from API

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((myresolve, myreject) => {
+    if (promise) {
+      myresolve({ status: 200, body: 'Success' });
+      console.log('Got a response from the API');
+    } else {
+      myreject(new Error(''));
+      console.log('Got a response from the API');
+    }
+  });
+}
