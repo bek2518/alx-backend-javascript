d769375a1a699e58a8ded8d8c84a636ff0337c57
bek2518 <bereketb20@hotmail.com>
2023-04-19 15:05:05 +0300

    Typo corrected

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((myresolve, myreject) => {
     if (success === true) {
-      myresolve({ status: 200, body: 'success' });
+      myresolve({ status: 200, body: 'Success' });
     } else if (success === false) {
       myreject(new Error('The fake API is not working currently'));
     }
