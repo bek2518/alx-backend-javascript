63f8daf9775b59c7799cd692bba5971c1c7c1d62
bek2518 <bereketb20@hotmail.com>
2023-04-20 15:29:35 +0300

    Eslint correction

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -43,12 +43,12 @@ export default class HolbertonCourse {
     if (Array.isArray(students)) {
       for (const student of students) {
         if (typeof (student) !== 'string') {
-          throw new TypeError('Students must be an array of strings')
+          throw new TypeError('Students must be an array of strings');
         }
       }
       this._students = students;
     } else {
-      throw new TypeError('Students must be an array of strings')
+      throw new TypeError('Students must be an array of strings');
     }
   }
 }
