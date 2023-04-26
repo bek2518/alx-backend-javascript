e901496cf45c9e8a959285433d11683084e01acd
bek2518 <bereketb20@hotmail.com>
2023-04-26 20:54:11 +0300

    Class StudentClass with interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,38 @@ export interface Teacher {
 export interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`
+}
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export interface studentConstructorInterface {
+  (firstName: string, lastName: string): studentClassInterface;
+}
+
+export interface studentClassInterface {
+  workOnHomeWork(): string;
+  displayName(): string;
+}
+
+export class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomeWork() {
+    return 'Currently working';
+  }
+
+  displayName(){
+    return this.firstName;
+  }
+}
