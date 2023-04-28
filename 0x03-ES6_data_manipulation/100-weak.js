959deec6d05f88e5b8c99bbf7ab200ace340e13b
bek2518 <bereketb20@hotmail.com>
2023-04-28 16:42:50 +0300

    100-weak passes test with decreased counter

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 let counter = 1;
 
 export function queryAPI(endPointArgument) {
-  if (weakMap.get(endPointArgument) >= 5) {
+  if (weakMap.get(endPointArgument) >= 4) {
     throw new Error('Endpoint load is high');
   }
   weakMap.set(endPointArgument, counter);
