f05b3e1ac847760fe117c7d91e819ba10f4ea1c0
bek2518 <bereketb20@hotmail.com>
2023-06-13 16:04:04 +0300

    Read a file synchronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw Error('Cannot load the database');
+  }
+
+  const fileContent = fs.readFileSync(path, 'utf8');
+  const fileContentArray = fileContent.split('\n');
+  const splitFileContent = [];
+
+  for (let i = 1; i < fileContentArray.length; i += 1) {
+    splitFileContent.push(fileContentArray[i].split(','));
+  }
+  const numberStudents = splitFileContent.length;
+  console.log(`Number of students: ${numberStudents}`);
+
+  const fieldList = [];
+  for (const student of splitFileContent) {
+    if (!fieldList.includes(student[3])) {
+      fieldList.push(student[3]);
+    }
+  }
+
+  for (const field of fieldList) {
+    const names = [];
+    for (const student of splitFileContent) {
+      if (student[3] === field) {
+        names.push(student[0]);
+      }
+    }
+    console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+  }
+};
